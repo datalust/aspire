2a7117a3f777319e6e2a9dfbb628f46f70710c01
Liam McLennan <liam.mclennan@gmail.com>
2024-02-09 11:10:31 +1000

    Added Seq component (client-side)

--- playground/seq/Seq.AppHost/SeqBuilderExtensions.cs
+++ playground/seq/Seq.AppHost/SeqBuilderExtensions.cs
@@ -3,10 +3,10 @@ namespace AspireSeq.AppHost;
 
 public static class SeqBuilderExtensions
 {
-    public static IResourceBuilder<SeqResource> AddSeq(this IDistributedApplicationBuilder builder, string name, bool acceptEula, int? port = null)
+    public static IResourceBuilder<SeqResource> AddSeq(this IDistributedApplicationBuilder builder, bool acceptEula, string name = "seq", int port = 5341)
     {
         return builder.AddResource(new SeqResource(name))
-            .WithEndpoint(hostPort: 5341, containerPort: 80, scheme: "http")
+            .WithEndpoint(hostPort: port, containerPort: 80, scheme: "http")
             .WithAnnotation(new ContainerImageAnnotation { Image = "datalust/seq", Tag = "latest" })
             .WithEnvironment("ACCEPT_EULA", acceptEula ? "Y" : "N");
     }
@@ -22,6 +22,6 @@ public class SeqResource(string name) : ContainerResource(name), IResourceWithCo
             throw new DistributedApplicationException("Seq resource does not have endpoint annotation.");
         }
 
-        return seqEndpointAnnotations.Single().EndPointString;
+        return seqEndpointAnnotations.Single().UriString;
     }
 }
