f93bab94c9b2a07d612c6e98aca23a81f823cc96
Liam McLennan <lmclennan@datalust.co>
2024-02-22 16:08:46 +1000

    Adding Seq tests

---
+++ tests/Aspire.Seq/AspireSeqExtensionsTests.cs
@@ -0,0 +1,9 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Seq.Tests;
+
+public class AspireSeqExtensionsTests
+{
+    // check that OTLP is configured correctly
+}
