8b3ab3c5b4eda9ac39e27196f6ad5e7d22622055
Liam McLennan <lmclennan@datalust.co>
2024-02-22 13:44:57 +1000

    Correct namespaces

--- src/Aspire.Hosting/Seq/SeqBuilderExtensions.cs
+++ src/Aspire.Hosting/Seq/SeqBuilderExtensions.cs
@@ -1,11 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
 using Aspire.Hosting.Publishing;
 
-namespace AspireSeq.AppHost;
+namespace Aspire.Hosting;
 
 /// <summary>
 ///
